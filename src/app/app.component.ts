f17af0f48675fa76404db66b57cd25c4e66fed5d
steph-codes <ogundele370@gmail.com>
2022-12-29 11:57:28 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
+  // template: `<h1>hello world from inline template</h1>
+  //         <p>Angular is Awesome</p>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
