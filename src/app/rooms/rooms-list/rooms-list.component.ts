9f86c9704716708f6c7f2931f92e31c749f1cfdd
steph-codes <ogundele370@gmail.com>
2022-12-30 04:51:59 +0100

    ngOnChange,DoCheck etc..

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,16 +1,28 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy,Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
 import {  RoomList } from '../rooms';
+
 
 @Component({
   selector: 'hinv-rooms-list',
   templateUrl: './rooms-list.component.html',
-  styleUrls: ['./rooms-list.component.scss']
+  styleUrls: ['./rooms-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
+
+ @Input() title: string = '';
 
   @Input() rooms: RoomList[] = [];
 
   @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if(changes['title']){
+      //updates rendered value when new data is passed
+      this.title = changes['title'].currentValue.toUppercase;
+    }
+  }
 
   ngOnInit(): void {
 
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { OnChanges } from '@angular/core';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent implements OnInit {
-
+export class RoomsComponent implements OnInit, DoCheck {
   hotelName = 'Hilton Hotel';
 
   numberOfRooms = 10;
@@ -16,55 +16,78 @@ export class RoomsComponent implements OnInit {
 
   selectedRoom!: RoomList;
 
-  rooms : Room  = {
-
+  rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
-  }
+  };
 
-  roomList : RoomList[] = []
+  title = "Room List";
+  roomList: RoomList[] = [];
 
   ngOnInit(): void {
-
     this.roomList = [
       {
         roomNumber: 23,
-        roomType : 'Deluxe Room',
+        roomType: 'Deluxe Room',
         amenities: 'Air Conditioner, Free WIFI, TV, Bathroom, Kitchen',
         price: 1000,
-        photos: 'https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-        checkinTime: new Date ('29-Dec-2022'),
-        checkoutTime: new Date ('30-Dec-2022'),
-        rating: 4.5
+        photos:
+          'https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        checkinTime: new Date('29-Dec-2022'),
+        checkoutTime: new Date('30-Dec-2022'),
+        rating: 4.5,
       },
       {
         roomNumber: 1,
-        roomType : 'Private Suite',
+        roomType: 'Private Suite',
         amenities: 'Air Conditioner, Free WIFI, TV, Bathroom, Kitchen',
         price: 1500,
-        photos: 'https://images.unsplash.com/photo-1602002418082-a4443e081dd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-        checkinTime: new Date ('29-Dec-2022'),
-        checkoutTime: new Date ('30-Dec-2022'),
-        rating: 4.9
+        photos:
+          'https://images.unsplash.com/photo-1602002418082-a4443e081dd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        checkinTime: new Date('29-Dec-2022'),
+        checkoutTime: new Date('30-Dec-2022'),
+        rating: 4.9,
       },
       {
         roomNumber: 2,
-        roomType : 'Executive Suite',
+        roomType: 'Executive Suite',
         amenities: 'Air Conditioner, Free WIFI, TV, Bathroom, Kitchen',
         price: 1500,
-        photos: 'https://images.unsplash.com/photo-1621293954908-907159247fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-        checkinTime: new Date ('29-Dec-2022'),
-        checkoutTime: new Date ('30-Dec-2022'),
-        rating: 3.7
-      }
+        photos:
+          'https://images.unsplash.com/photo-1621293954908-907159247fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        checkinTime: new Date('29-Dec-2022'),
+        checkoutTime: new Date('30-Dec-2022'),
+        rating: 3.7,
+      },
     ];
   }
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = "Rooms List";
   }
 
-  selectRoom(room: RoomList){
+  ngDoCheck(){
+
+  }
+  selectRoom(room: RoomList) {
     this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 9,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free WIFI, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      checkinTime: new Date('29-Dec-2022'),
+      checkoutTime: new Date('30-Dec-2022'),
+      rating: 4.5,
+    };
+
+    //this.roomList.push(room);
+    this.roomList=[...this.roomList, room];
   }
 }
